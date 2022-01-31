e2f07e06c1069fd59091e58e1a8546c2fc74c43e
Victor_Gostyaikin <gostyaikin@yandex.ru>
2022-01-31 20:28:55 +0300

    isort

--- backend/api/recipes/serializers.py
+++ backend/api/recipes/serializers.py
@@ -1,5 +1,4 @@
 ''' Serializers for 'recipes' API application. '''
-
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from rest_framework.validators import ValidationError
--- backend/users/management/commands/load_users.py
+++ backend/users/management/commands/load_users.py
@@ -1,6 +1,7 @@
 import csv
 
 import requests
+
 from django.core.management.base import BaseCommand
 from users.models import User
 
